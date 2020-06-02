7e67dc63ade7a07788b7677e5746e639b798c5e7
Christian Lewis <ctjl96@gmail.com>
2020-06-02 18:25:58 -0500

    BULK AUTOMATIC COMMIT

--- cli/command/bin/clean.js
+++ cli/command/bin/clean.js
@@ -1,8 +1,10 @@
 const rmrf = require('rmrf');
+const fs = require('fs');
 const { projectCheck, rocketLog, spawnGatsby } = require('../../utils/processes');
 
 const clean = async () => {
-    if (!projectCheck()) return;
+    if (!projectCheck() || !fs.existsSync('./build/src')) 
+        return console.log("Not in project or build dir does not exist.");
 
     rocketLog("Cleaning project...");
     await spawnGatsby("clean");
